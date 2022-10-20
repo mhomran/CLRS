33a446f7873510579d46fc4290d63f8f5c253510
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2022-10-21 00:34:12 +0200

    Add: array sequence implementation for the Set ADT.

---
+++ set/array_set.hpp
@@ -0,0 +1,215 @@
+#include <climits>
+#include "set.hpp"
+#include "../sequence/array_seq.hpp"
+#include "../sorting/merge_sort.hpp"
+
+template <class T>
+class ArraySet {
+    ArraySeq<Pair<T>> array;
+
+    int Abs(int a) {
+        if (a > 0) return a;
+        else return -a;
+    }
+
+    int LinearSearch(int k) {
+        int res;
+        int i;
+
+        res = -1;
+
+        for(i = 0; i < array.GetSize(); i++) {
+            if(array.GetAt(i).GetKey() == k) {
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public:
+    /**
+     * @brief given an iterable X, build set from items in X
+     * 
+     * @param X 
+     * @param size 
+     */
+    virtual void Build(Pair<T> X[], int size) {
+        array.Build(X, size);
+    }
+
+    /**
+     * @brief return the stored item with key k
+     * 
+     * @param k 
+     */
+    virtual Pair<T> Find(int k) {
+        Pair<T> res;
+        int i;
+        
+        i = LinearSearch(k);
+        if(-1 != i) {
+            res = array.GetAt(i);
+        } else {
+            res.SetKey(-1);
+        }
+        return res;
+    }
+
+    /**
+     * @brief add x to set (replace item with key x.key if one already exists) 
+     * 
+     * @param x
+     */
+    virtual void Insert(Pair<T> x) {
+        int i;
+
+        i = LinearSearch(x.GetKey());
+        if (-1 != i) {
+            array.SetAt(i, x);
+        } else {
+            array.InsertLast(x);
+        }
+    }
+
+    /**
+     * @brief remove and return the stored item with key k
+     * 
+     * @param k 
+     */
+    virtual void Delete(int k) {
+        int i;
+        i = LinearSearch(k);
+        if(-1 != i) {
+            array.DeleteAt(i);
+        } 
+    }
+
+    /**
+     * @brief return the stored item with smallest key
+     * 
+     */
+    virtual Pair<T> FindMin(void) {
+        Pair<T> res;
+        int resI;
+        int i;
+        int minKey;
+
+        resI = -1;
+        minKey = INT_MAX;
+        for(i = 0; i < array.GetSize(); i++) {
+            if(array.GetAt(i).GetKey() < minKey) {
+                resI = i;
+                minKey = array.GetAt(i).GetKey();
+            }
+        }
+
+        if(-1 != resI) {
+            res = array.GetAt(resI);
+        } else {
+            res.SetKey(-1);
+        }
+        return res;
+    }
+
+    /**
+     * @brief return the stored item with largest key
+     * 
+     */
+    virtual Pair<T> FindMax(void) {
+        Pair<T> res;
+        int resI;
+        int i;
+        int maxKey;
+
+        resI = -1;
+        maxKey = INT_MIN;
+        for(i = 0; i < array.GetSize(); i++) {
+            if(array.GetAt(i).GetKey() > maxKey) {
+                resI = i;
+                maxKey = array.GetAt(i).GetKey();
+            }
+        }
+
+        if(-1 != resI) {
+            res = array.GetAt(resI);
+        } else {
+            res.SetKey(-1);
+        }
+        return res;
+    }
+
+    /**
+     * @brief return the stored item with smallest key larger than k
+     * 
+     * @param k 
+     */
+    virtual Pair<T> FindNext(int k) {
+        Pair<T> res;
+        int i;
+        int nextKeyDiff = INT_MAX;
+        int diff;
+        int resI;
+
+        resI = -1;
+        for(i = 0; i < array.GetSize(); i++) {
+            diff = array.GetAt(i).GetKey() - k;
+            if(diff > 0 && diff < nextKeyDiff) {
+                resI = i;
+                nextKeyDiff = diff;
+            }
+        }
+
+        if(-1 != resI) {
+            res = array.GetAt(resI);
+        } else {
+            res.SetKey(-1);
+        }
+
+        return res;
+    }
+
+    /**
+     * @brief return the stored item with largest key smaller than k
+     * 
+     * @param k 
+     */
+    virtual Pair<T> FindPrev(int k) {
+        Pair<T> res;
+        int i;
+        int nextKeyAbsDiff = INT_MAX;
+        int diff;
+        int resI;
+
+        resI = -1;
+        for(i = 0; i < array.GetSize(); i++) {
+            diff = array.GetAt(i).GetKey() - k;
+            if(diff < 0 && Abs(diff) < nextKeyAbsDiff) {
+                resI = i;
+                nextKeyAbsDiff = Abs(diff);
+            }
+        }
+
+        if(-1 != resI) {
+            res = array.GetAt(resI);
+        } else {
+            res.SetKey(-1);
+        }
+
+        return res;
+    }
+
+    /**
+     * @brief Print the Set
+     * 
+     */
+    virtual void Print(void) {
+        array.Print();
+    }
+
+    /**
+     * @brief Destroy the Set object
+     * 
+     */
+    virtual ~ArraySet() {};
+};
---
+++ set/test_array_set.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "array_set.hpp"
+using namespace std;
+
+int main(void) {
+    ArraySet<int>* A = new ArraySet<int>();
+    int i;
+    Pair<int> X[7];
+    Pair<int> x;
+    int Keys[] = { 9, 2, 3, 7, 5, 6, 8, 1, 0 };
+    int Keys_size = sizeof(Keys)/sizeof(Keys[0]);
+
+
+    for(i = 0; i < Keys_size; i++) {
+        X[i].SetKey(Keys[i]);
+        X[i].SetItem(700);
+    }
+
+    cout << "Testing Build(X, 7):\n";
+    A->Build(X, 7);
+    A->Print();
+    
+    cout << "Testing Find(4):\n";
+    cout << A->Find(4) << endl;
+
+    cout << "Testing Find(30):\n";
+    cout << A->Find(30) << endl;
+
+    cout << "Testing Find(3):\n";
+    cout << A->Find(3) << endl;
+
+    cout << "Testing FindNext(3):\n";
+    cout << A->FindNext(3) << endl;
+
+    cout << "Testing FindNext(30):\n";
+    cout << A->FindNext(30) << endl;
+
+    cout << "Testing FindPrev(3):\n";
+    cout << A->FindPrev(3) << endl;
+
+    cout << "Testing FindPrev(30):\n";
+    cout << A->FindPrev(30) << endl;
+ 
+    cout << "Testing FindMin():\n";
+    cout << A->FindMin() << endl;
+
+    cout << "Testing FindMax():\n";
+    cout << A->FindMax() << endl;
+
+    x.SetKey(3);
+    x.SetItem(500);
+    cout << "Testing Insert(x) x is (3, 500):\n";
+    A->Insert(x);
+    A->Print();
+
+    x.SetKey(4);
+    x.SetItem(700);
+    cout << "Testing Insert(x) x is (4, 700):\n";
+    A->Insert(x);
+    A->Print();
+
+    cout << "Testing Delete(3):\n";
+    A->Delete(3);
+    A->Print();
+
+    delete A;
+}
